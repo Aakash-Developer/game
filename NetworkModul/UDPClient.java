06650fb7935c353c5e4ab03ee6aca44230ee25d3
puk18 <wadhwa.pulkit18@gmail.com>
2019-08-02 20:57:13 -0400

    Updated module

--- NetworkModul/UDPClient.java
+++ NetworkModul/UDPClient.java
@@ -1,4 +1,54 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
 public class UDPClient {
 
+	public static void main(String[] args) throws Exception {
+		UDPClient.sendMessage(1313,"SenderA:shello");//calling send message function
+	}
+	// send message function
+	public static String sendMessage(int serverPort,String text) {
+		
+		DatagramSocket aSocket = null;
+        
+		try {
+           
+			aSocket = new DatagramSocket();
+            String mess=text;
+            
+			byte[] message = mess.getBytes();
+
+			InetAddress aHost = InetAddress.getByName("230.1.1.5");
+
+			DatagramPacket request = new DatagramPacket(message, message.length, aHost, serverPort);
+			aSocket.send(request);
+
+			System.out.println(" message sent is: "
+
+					+ new String(request.getData()));
+		} catch (SocketException e) {
+
+			System.out.println("Socket: " + e.getMessage());
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+			System.out.println("IO: " + e.getMessage());
+
+		} finally {
+
+			if (aSocket != null)
+
+				aSocket.close();
+
+		}
+		return null;
+
+	}
 }
+
+
--- NetworkModul/UDPServer.java
+++ NetworkModul/UDPServer.java
@@ -1,4 +1,87 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketException;
 
 public class UDPServer {
+	
+	// receiver to receive the message which will always be active 
+	public static void receive() {
+		
+		
+		MulticastSocket aSocket = null;
 
-}
+			try {
+				
+		    	
+
+				aSocket = new MulticastSocket(1313);
+
+				aSocket.joinGroup(InetAddress.getByName("230.1.1.5"));
+
+				System.out.println(" Started............");
+				
+				while (true) {
+					
+					byte[] buffer = new byte[1000];
+
+					DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+
+					aSocket.receive(request);
+					
+					String stringdata=new String (request.getData());
+					System.out.println(stringdata);
+			
+					DatagramPacket reply = new DatagramPacket(buffer, buffer.length, request.getAddress(),
+
+							request.getPort());
+
+					aSocket.send(reply);
+					
+		
+	}
+			
+
+	} catch (SocketException e) {
+		System.out.println("Socket: " + e.getMessage());
+
+	} catch (IOException e) {
+
+		System.out.println("IO: " + e.getMessage());
+
+	} finally {
+
+		if (aSocket != null)
+			System.out.println("xyz");
+			aSocket.close();
+
+	}
+		//	return null;
+
+	}
+	public static void startReceiver() {
+
+		try{
+	    	
+			
+			Runnable task = () -> {
+
+				receive();
+
+			};
+		
+			Thread thread = new Thread(task);
+			
+			thread.start();
+		
+	}
+		catch (Exception re) {
+	    System.out.println("Exception : " + re);
+	   // logger.info("Exception in ServerComp: " + re);
+		} 
+		}
+	public static void main(String... s) {
+		UDPServer.startReceiver();
+	}
+	}
